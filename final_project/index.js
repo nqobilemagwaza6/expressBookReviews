e6331c2137eeec048e2d2859939b71561d298a84
Nqobile <111858362+nqobilemagwaza6@users.noreply.github.com>
2026-03-17 15:01:25 +0200

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,11 +12,25 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+const accessToken = req.headers['authorization']; // get token from header
+if (!accessToken) {
+    return res.status(401).json({ message: "Access token missing" });
+}
+
+const sessionUser = req.session.authorization && req.session.authorization[accessToken];
+if (!sessionUser) {
+    return res.status(403).json({ message: "User not authenticated" });
+}
+
+// user is authenticated
+next();
 });
  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
+app.get("/", (req, res) => res.send("Book Review API is running!"));
+
 
 app.listen(PORT,()=>console.log("Server is running"));
